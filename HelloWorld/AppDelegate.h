ce43575f7703c445703d8379dd98484b2c536662
Victor B. Mattaur <vmattaur@Vic.local>
2013-10-30 22:50:39 -0500

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Victor B. Mattaur on 10/30/13.
+//  Copyright (c) 2013 Victor B. Mattaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Victor B. Mattaur on 10/30/13.
+//  Copyright (c) 2013 Victor B. Mattaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
